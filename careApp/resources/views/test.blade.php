2e984075e169b2907804f8c627f6f621d057b3e0
JANANI <jananiselvam15@gmail.com>
2023-04-25 22:24:46 +0100

    added date picker in test view blade

--- careApp/resources/views/test.blade.php
+++ careApp/resources/views/test.blade.php
@@ -1,46 +1,54 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
+
 <head>
+    <title>Datepicker</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Datepicker</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 </head>
 
-<body>
-    <main class="container">
-        <header class="col-sm-12">
-            <img src="./images/banner.png" alt="Page Banner" width="800px" height="130px">
-            <span>Bootstrap Calender</span>
-        </header>
-        <section class="col-sm-12">
-            <div class="form-group">
-                <div class='input-group date' id='datetimepicker1'>
-                    <input type='text' class="form-control" />
-                    <span class="input-group-addon">
-                            <span class="glyphicon glyphicon-calendar"></span>
-                    </span>
+<body style="background-color: white;">
+    <section class="container">
+    <h2 class = "mt-4" style= "font-family: Georgia;"><center>Please choose the Date and the Preference below</center></h2>
+        <form action="/save-date" method="POST">
+            <div class="row form-group">
+                <label for="date" class="col-sm-1 col-form-label">Date:</label>
+                <div class="col-sm-4">
+                    <div class="input-group date" id="datepicker">
+                    <input type="text" class="form-control" name="date">
+                        <span class="input-group-append">
+                            <span class="input-group-text bg-white">
+                                <i class="fa fa-calendar"></i>
+                            </span>
+                        </span>
+                    </div>
                 </div>
             </div>
+        </form>
+    </section>
 
-        </section>
-        <footer>
-
-        </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    </main>
     <script type="text/javascript">
-        $(function() {
-            $('#datetimepicker1').datetimepicker();
-        });
+       $(function() {
+    $('#datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true,
+        todayHighlight: true,
+        startDate: new Date(),
+        endDate: '+1m',
+    }).on('changeDate', function() {
+        $(this).closest('form').submit();
+    });
+});
     </script>
+
+
 </body>
 
 </html>
